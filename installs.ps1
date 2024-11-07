8b2e202af2d7cdac2ec3286d23ec0286003f666e
Bennett Smrdel <43832671+bsmrdel101@users.noreply.github.com>
2024-11-07 15:35:08 -0600

    Update installs.ps1

--- installs.ps1
+++ installs.ps1
@@ -59,3 +59,4 @@ pacman -Sy git curl zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 Write-Output "All applications installed successfully."
+pause
